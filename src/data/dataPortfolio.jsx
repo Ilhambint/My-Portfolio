26ceb484f254d75bd261b40992c8fb60d693d52d
Ilhambint <99519666+Ilhambint@users.noreply.github.com>
2024-11-20 20:59:24 +0700

    master

--- src/data/dataPortfolio.jsx
+++ src/data/dataPortfolio.jsx
@@ -8,18 +8,21 @@ export const portfoliolist = [
         id: '1',
         title: 'website Portfolio #1',
         image: portfolio1,
-        skill: 'Slicing website that can slicing using HTML, CSS, and Javascript.'
+        skill: 'Slicing website that can slicing using HTML, CSS, and Javascript.',
+        link: 'https://ilhambint-portfolio-car-rental.vercel.app/'
     }, 
     {
         id: '2',
         title: 'website Portfolio #2',
         image: portfolio2,
-        skill: 'The website creates a simple burger using React.js and Javascript.'
+        skill: 'The website creates a simple burger using React.js and Javascript.',
+        link: 'https://ilhambint-burger-mini-app.vercel.app/'
     }, 
     {
         id: '3',
         title: 'website Portfolio #3',
         image: portfolio3,
-        skill: 'Learn to create an e-commerce website using React.Js'
+        skill: 'Learn to create an e-commerce website using React.Js from youtube.',
+        link: 'https://e-ecommerce-git-main-vexas-projects.vercel.app/'
     }
 ]
